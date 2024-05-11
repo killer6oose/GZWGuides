f2d507985b89632da4cbaf43db7de19dc30b20e1
Andrew <andrew.hatton@cronotech.us>
2024-05-11 07:30:23 -0400

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+document.getElementById('vendors').addEventListener('click', function() {
+    alert('Vendors Dropdown');
+});
+
+document.getElementById('maps').addEventListener('click', function() {
+    alert('Maps Dropdown');
+});
+
+document.getElementById('guides').addEventListener('click', function() {
+    alert('Guides Page');
+});
+
+document.getElementById('misc').addEventListener('click', function() {
+    alert('Misc Dropdown');
+});
